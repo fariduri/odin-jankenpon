0997d96d22e355270bc737bc5276cd18a3812c1f
fariduri <fadriduck@gmail.com>
2022-04-27 15:39:42 +0700

    Refine randomizer, add logic skeleton

--- game-logic.js
+++ game-logic.js
@@ -2,8 +2,24 @@ console.log("遊びましょう！");
 
 function computerPlay() // Returns Computer Choice
 {
-    let randomPicker = Math.floor(Math.random()*10);
+    const min = 1; // min random integer range
+    const max = 10; // max random integer range
+
+    let randomPicker = Math.floor(Math.random() * (max - min)) + min; // Gives random integer between 1 and 9 (both included)
     console.log(randomPicker);
+
+    if(1 <= randomPicker < 4)
+    {
+
+    } 
+    else if(4 <= randomPicker < 7)
+    {
+
+    }
+    else if(7 <= randomPicker < 10)
+    {
+
+    }
     return null;
 }
 
