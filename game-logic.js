04e70c653d01fa8438ccc316837d353b69d3ba3f
fariduri <fadriduck@gmail.com>
2022-04-27 15:20:45 +0700

    Setup Basic Functions

--- game-logic.js
+++ game-logic.js
@@ -1,1 +1,16 @@
 console.log("遊びましょう！");
+
+function computerPlay() // Returns Computer Choice
+{
+    return null;
+}
+
+function playRound(playerSelection, computerSelection) // Play one round
+{
+
+}
+
+function game() // Run game, play 5 rounds
+{
+
+}
