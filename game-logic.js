372257f55710f5e5a7675d1294e39345d5a6b616
fariduri <fadriduck@gmail.com>
2022-04-27 15:27:43 +0700

    Add & Test randomizer

--- game-logic.js
+++ game-logic.js
@@ -2,6 +2,8 @@ console.log("遊びましょう！");
 
 function computerPlay() // Returns Computer Choice
 {
+    let randomPicker = Math.floor(Math.random()*10);
+    console.log(randomPicker);
     return null;
 }
 
@@ -14,3 +16,7 @@ function game() // Run game, play 5 rounds
 {
 
 }
+
+computerPlay();
+computerPlay();
+computerPlay();
