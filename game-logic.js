2d44d01477fc7e8394259054332e6627553a4a03
fariduri <63889035+fariduri@users.noreply.github.com>
2022-07-27 22:00:55 +0700

    Make Readable and simpler

--- game-logic.js
+++ game-logic.js
@@ -1,89 +1,83 @@
-console.log("遊びましょう！");
-
-// Choices
 const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 
-function computerPlay() // Returns Computer Choice
+const PLAYERWINS = "PLAYERWINS";
+const DRAW = "DRAW";
+const COMPUTERWINS = "COMPUTERWINS";
+
+function getComputerChoice()
 {
-    const MIN = 1; // min random integer range
-    const MAX = 10; // max random integer range
+    let randNum = Math.random();
 
-    let randomPicker = Math.floor(Math.random() * (MAX - MIN)) + MIN; // Gives random integer between 1 and 9 (both included)
-
-    if(randomPicker >= 1 && randomPicker < 4)
-        return(ROCK);
-    else if(randomPicker >= 4 && randomPicker < 7)
-        return(PAPER);
-    else
-        return(SCISSORS);
-        
+    switch (true) {
+        case randNum < 0.33334:
+            return ROCK;
+        case randNum < 0.66667:
+            return PAPER;
+        default:
+            return SCISSORS;
+    }
 }
 
-function playRound(playerSelection, computerSelection) // Play one round
+function playRound(playerHand, computerHand)
 {
-    let score; // Array to store scores [0: Player, 1: Computer]
+    console.log(`player: ${playerHand} vs computer: ${computerHand}`);
 
-    // Game logic
-    if (playerSelection == computerSelection) // if (player = computer)
+    if ((playerHand === ROCK && computerHand === SCISSORS) || (playerHand === PAPER && computerHand === ROCK) || (playerHand === SCISSORS && computerHand === ROCK))
     {
-        console.log(`Issa Draw\nYou: ${playerSelection}\t Computer: ${computerSelection}`); // say "Issa draw"
-        score = [1,1]; // return [1,1]
-    } 
-    else if ((playerSelection == ROCK && computerSelection == SCISSORS) 
-            || (playerSelection == PAPER && computerSelection == ROCK) 
-            || (playerSelection == SCISSORS && computerSelection == PAPER)) // else if ((player = ROCK and computer = scissors) or (player = PAPER and computer = ROCK) or (player = SCISSSORS and computer = PAPER))
-    {
-        console.log(`You Win!!!!\nYou: ${playerSelection}\t Computer: ${computerSelection}`); // say "You Win!"
-        score = [1,0]; // return [1,0]
+        console.log(`You Win! ${playerHand} beats ${computerHand}`);
+        return PLAYERWINS;
     }
-    else // else
+    else if (playerHand === computerHand)
     {
-        console.log(`L\nYou: ${playerSelection}\t Computer: ${computerSelection}`); // say "You Lose!"
-        score = [0,1]; // return [0,1]
+        console.log(`Draw! ${playerHand} equals ${computerHand}`);
+        return DRAW;
     }
-
-    return score;
-
+    else 
+    {
+        console.log(`You Lose! ${computerHand} beats ${playerHand}`);
+        return COMPUTERWINS;
+    }
 }
 
-function game() // Run game, play 5 rounds
+function game()
 {
-    let playerInput;
-    let computerInput;
-    let scorePlayer = 0, scoreComputer = 0; // Store overall score [0: Player, 1: Computer]
-    let updateScore; // Array to store to ber added score [0: Player, 1: Computer]
-    
-    // Game loop
-    for (let round = 1; round < 6; round++) // Loop 5 times
-    {
-        console.log(`:::::::::  Round ${round}  ::::::::`)
-        computerInput = computerPlay(); // Ask Computer Input
-        playerInput = (prompt(`Round ${round}\nChoose: Rock, Paper, Scissors | Not case sensitive!`)).toUpperCase();
+    let computerHand, playerHand, result;
+    let playerScore = 0;
+    let computerScore = 0;
 
-        while(playerInput != ROCK && playerInput != PAPER && playerInput != SCISSORS)// Ask Player Input, if input not Rock Paper or Scissors, ask again
+    console.log("遊びましょう！");
+
+    for (let round = 1; round <= 5; round++)
+    {
+        computerHand = getComputerChoice().toUpperCase();
+        console.log(`::::: Round ${round} :::::`);
+
+        do {
+            playerHand = prompt(`::::: Round ${round} :::::\nChoose Your Weapon!\n 'ROCK', 'PAPER', 'SCISSORS'`).toUpperCase();
+        } while (playerHand !== ROCK && playerHand !== PAPER && playerHand !== SCISSORS);
+
+        result = playRound(playerHand, computerHand);
+
+        switch(result)
         {
-            playerInput = (prompt(`Invalid Input!\nRound ${round}\nChoose: Rock, Paper, Scissors | Not case sensitive!`)).toUpperCase();
+            case DRAW:
+                playerScore++;
+                computerScore++;
+                break;
+            case PLAYERWINS:
+                playerScore++;
+                break;
+            case COMPUTERWINS:
+                computerScore++;
+                break;    
         }
-        
-        updateScore = playRound(playerInput, computerInput); // determine who won
-        
-        // Update Score
-        scorePlayer += updateScore[0]; 
-        scoreComputer += updateScore[1];
-
-        console.log(`Current Score\nYou: ${scorePlayer} Computer: ${scoreComputer}`);
     }
 
-    console.log(':::::::  Final Verdict  ::::::');
-    if (scorePlayer > scoreComputer)
-        console.log("YOU WON THE MATCH!!");
-    else if (scorePlayer < scoreComputer)
-        console.log("MATCH LLLLLLLLLLLLL");
-    else
-        console.log("DRAAAAAAAAWWW LLLLLLLLL");
-    
+    let whoWon = (playerScore>computerScore) ? "You Won!": (playerScore<computerScore) ? "You Lost!" : "It's a Draw!";
+    let message = `${whoWon}\n\nFinal Score\nPlayer: ${playerScore} Computer: ${computerScore}`;
+    alert(message);
 }
 
 game();
